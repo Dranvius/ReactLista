5a74050afad29354032bcf3bc6cec07fe5d4af4f
Dranvius <dranvius12@hotmail.com>
2022-12-16 18:53:45 -0500

    Inicio del proyecto

---
+++ vite-proyect/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <h1>Hola mundo</h1>
+      <ul>
+        <li><a href="">Hola</a></li>
+        <li><a href="">Napy</a></li>
+        <li><a href="">Chanchito feliz</a></li>
+      </ul>
+    </div>
+  )
+
+
+}
+
+export default App
